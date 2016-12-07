b9e15af39f00c13441350579f3e6357466ab2445
Combinations <jamesleahy314@gmail.com>
2016-12-07 00:49:33 -0800

    First commit

---
+++ trending_intersection.py
@@ -0,0 +1,27 @@
+import twitter
+import json
+# Simple script that uses twitters API to find the intersection between the most popular trends in the US and the world. 
+CONSUMER_KEY = "g1bRM1DVylg5XTa01CPgMVuyr"
+CONSUMER_SECRET = "8sYqjVCBsXDRgxKBIlUyF6ZTUUkXuZU5s0D1Lod1d8ZUQzh4Ji"
+OAUTH_TOKEN = "801509424472866816-HWwd0Fas1wzPwrSg8AfOZdiIn4A6tJE"
+OAUTH_TOKEN_SECRET = "3kt0xZf3BhpfwfC7l65N4PPSSHhi8wwaShCHricn0KF2b"
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+ CONSUMER_KEY, CONSUMER_SECRET)
+# The Yahoo! Where On Earth ID for the entire world is 1.
+# See https://dev.twitter.com/docs/api/1.1/get/trends/place and
+# http://developer.yahoo.com/geo/geoplanet/
+twitter_api = twitter.Twitter(auth=auth)
+WORLD_WOE_ID = 1
+US_WOE_ID = 23424977
+# Prefix ID with the underscore for query string parameterization.
+# Without the underscore, the twitter package appends the ID value
+# to the URL itself as a special case keyword argument.
+world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
+us_trends = twitter_api.trends.place(_id=US_WOE_ID)
+world_trends_set = set([trend['name']
+ for trend in world_trends[0]['trends']])
+us_trends_set = set([trend['name']
+ for trend in us_trends[0]['trends']])
+common_trends = world_trends_set.intersection(us_trends_set)
+print common_trends
+
